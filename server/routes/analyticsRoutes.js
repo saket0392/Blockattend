248436086ba4374f12725084a4a9612e5eaffb7a
Saket Srivastava <saketkumar4286@gmail.com>
2026-02-28 16:49:40 +0530

    backend done

--- server/routes/analyticsRoutes.js
+++ server/routes/analyticsRoutes.js
@@ -17,7 +17,7 @@ router.get("/:id/analytics", async (req, res) => {
     const analytics = getAttendanceAnalytics(
       totalClasses,
       attendedClasses,
-      10 
+      remainingClasses = 100 - totalClasses,
     );
 
     res.json({
