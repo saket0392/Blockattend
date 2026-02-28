306b06e19cb0c5fcf78f3db2d38b73b5a9531e24
Saket Srivastava <saketkumar4286@gmail.com>
2026-02-28 12:59:26 +0530

    models and controllers done

--- server/Server.js
+++ server/Server.js
@@ -2,11 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const connectDb = require("./config/db");
 const app = express();
-
+const sessionRoutes = require("./routes/sessionRoutes")
 connectDb();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/session", sessionRoutes);
 app.get('/', (req, res) => {
    res.send("Blockattend Backend Running");
 });
---
+++ server/controllers/sessionController.js
@@ -0,0 +1,35 @@
+const Session = require("../models/Sessions");
+const { v4: uuidv4 } = require("uuid");
+
+exports.createSession = async (req, res) => {
+  try {
+    const { subject, facultyName } = req.body;
+
+    if (!subject || !facultyName) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    const sessionId = uuidv4();
+    const nonce = uuidv4();
+
+    const expiryTime = new Date(Date.now() + 90 * 1000); 
+
+    const session = await Session.create({
+      sessionId,
+      subject,
+      facultyName,
+      expiryTime,
+      nonce,
+      isActive: true
+    });
+
+    res.status(201).json({
+      sessionId: session.sessionId,
+      nonce: session.nonce,
+      expiryTime: session.expiryTime
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
